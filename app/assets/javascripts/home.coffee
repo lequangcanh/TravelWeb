41cbbbaf2dd27bd6d6202615a18863813a8cb728
hatv <hatv@asinantech.vn>
2016-11-08 23:23:35 +0700

    recent places

--- app/assets/javascripts/home.coffee
+++ app/assets/javascripts/home.coffee
@@ -4,3 +4,10 @@ $('.home.index').ready ->
     arrows: false,
     dots: false
   })
+
+  $('.recent-places').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true
+  })
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @famous_places = Place.all.order('view_count desc, place_comments_count desc').limit(3)
+    @recent_places = Place.all.order('created_at desc').limit(20)
   end
 end
