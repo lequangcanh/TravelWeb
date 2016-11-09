2c910a044d97ff25af793903bab4f7abf226684a
lequangcanh <lequangcanh94@gmail.com>
2016-11-09 16:37:12 +0700

    Basic admin/places controller

---
+++ app/controllers/admin/places_controller.rb
@@ -0,0 +1,47 @@
+class Admin::PlacesController < ApplicationController
+  layout 'admin/layouts/admin_panel'
+
+  def index
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.create(place_params)
+    if @place.save
+      redirect_to admin_place_path(@place)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.update_attributes(place_params)
+      redirect_to admin_place_path(@place)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Place.find(params[:id]).destroy
+    redirect_to admin_places_path
+  end
+
+  private 
+  def place_params
+    params.require(:place).permit(:name, :avatar, :description, :province_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   	root 'home#index'
     resources :users
     resources :provinces
+    resources :places
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
