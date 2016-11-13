3c52953944d9d087ec39d82b772ea44ba71a13a1
hatv <hatv@asinantech.vn>
2016-11-13 17:12:28 +0700

    fix homepage js, limit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  resources :places do
-    resources :place_comments
+  resources :places, only: [:index, :show] do
+    resources :place_comments, only: [:create, :destroy, :update]
   end
 
   devise_for :users
