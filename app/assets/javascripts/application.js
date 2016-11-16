2fe404b017f533b79a4081ba5c17aa19f876d268
hatv <hatv@asinantech.vn>
2016-11-16 18:35:43 +0700

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
+//= require_tree .
 //= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require slick-carousel/slick/slick.min
 //= require bootstrap-fileinput/js/plugins/canvas-to-blob.min.js
