b111366d42c9159ce1c76bf26bc40b2a38c5b8f6
hatv <hatv@asinantech.vn>
2016-11-16 18:12:30 +0700

    assets

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module TravelWeb
 
     # Precompile Bootstrap fonts
     config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
-    config.assets.precompile << %r(assets/javascripts/[\w-]*/*[\w-]+\.(?:coffee?)$)
+    config.assets.precompile << %r(app/assets/javascripts/[\w-]*/*[\w-]+\.(?:coffee?)$)
     # Minimum Sass number precision required by bootstrap-sass
     ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
 
